b5a56bb08d7f85c70cfe1eb00f9433b621df0081
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2023-12-20 13:10:55 +0000

    Make autochord consume less gpu

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,16 @@
-import autochord
 import io
 import whisper
 import pandas as pd
 import subprocess
 from .formatter import Formatter
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+import autochord
 
 def get_free_gpus():
     gpu_stats = subprocess.check_output(["nvidia-smi", "--format=csv", "--query-gpu=memory.used,memory.free"], text=True)
@@ -11,25 +18,26 @@ def get_free_gpus():
                          names=['memory.used', 'memory.free'],
                          skiprows=1)
     gpu_df['memory.free'] = gpu_df['memory.free'].map(lambda x: x.rstrip(' [MiB]')).astype(int)
-    idx = gpu_df.nlargest(2, ['memory.free']).index
-    return tuple(idx)
+    idx = gpu_df.nlargest(1, ['memory.free']).index
+    return idx[0]
 
 class Backend:
     def __init__(self) -> None:
         self.formatter = Formatter()
-        gpu1, gpu2 = get_free_gpus()
-        self.model = whisper.load_model("medium", device=f"cuda:{gpu1}")
+        gpu = get_free_gpus()
+        self.model = whisper.load_model("medium", device=f"cuda:{gpu}")
 
-        self.model.encoder.to(f"cuda:{gpu1}")
-        self.model.decoder.to(f"cuda:{gpu2}")
-        self.model.decoder.register_forward_pre_hook(lambda _, inputs: tuple([inputs[0].to(f"cuda:{gpu2}"), inputs[1].to(f"cuda:{gpu2}")] + list(inputs[2:])))
-        self.model.decoder.register_forward_hook(lambda _, inputs, outputs: outputs.to(f"cuda:{gpu1}"))
+        # Moving encoder and decoder to separate gpus
+        # self.model.encoder.to(f"cuda:{gpu1}")
+        # self.model.decoder.to(f"cuda:{gpu2}")
+        # self.model.decoder.register_forward_pre_hook(lambda _, inputs: tuple([inputs[0].to(f"cuda:{gpu2}"), inputs[1].to(f"cuda:{gpu2}")] + list(inputs[2:])))
+        # self.model.decoder.register_forward_hook(lambda _, inputs, outputs: outputs.to(f"cuda:{gpu1}"))
 
     def query_pdf(self, *, audio: str, add_tabs: bool) -> io.BytesIO:
         ...
 
     def query_txt(self, *, audio: str) -> io.BytesIO:
-        return io.BytesIO(bytes(self.query(audio), 'utf-8'))
+        return io.BytesIO(bytes(self.query(audio=audio), 'utf-8'))
 
     def query(self, *, audio: str) -> str:
         text = self.model.transcribe(audio=audio)
