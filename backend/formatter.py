8bf3bff419572baac620d8a8ed72abdc31131f1f
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2023-12-20 07:34:41 +0000

    Add whisper support and basic formatter

--- backend/formatter.py
+++ backend/formatter.py
@@ -1,9 +1,35 @@
+from dataclasses import dataclass, asdict
+import enum
+from operator import attrgetter
+import json
+
+class EventType(enum.IntEnum):
+    chord = 1
+    phrase = 2
+
+@dataclass(slots=True)
+class Event:
+    start: int
+    end: int
+    type: EventType
+    content: str
+
 class Formatter:
     def __init__(self) -> None:
         ...
 
-    def format(self, chords) -> str:
-        for i in range(len(chords)):
-            chords[i] = list(chords[i])
-            chords[i][-1] = chords[i][-1][:-4] + ('m' if chords[i][-1][-3:] == 'min' else '')
-        return ...
+    def format(self, chords, text) -> str:
+        events: list[Event] = []
+        for chord in chords:
+            content = chord[2][:-4]
+            if chord[2].endswith("min"):
+                content += "m"
+            events.append(Event(start=chord[0], end=chord[1], type=EventType.chord, content=content))
+                          
+        for seg in text["segments"]:
+            events.append(Event(start=seg["start"], end=seg["end"], type=EventType.phrase, content=seg["text"]))
+            
+        
+        events.sort(key=attrgetter("start"))
+
+        return json.dumps(events, indent=4, default=lambda o: asdict(o))
--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,18 @@
 import io
 import autochord
+import whisper
 from .formatter import Formatter
 
 
 class Backend:
     def __init__(self) -> None:
         self.formatter = Formatter()
+        self.model = whisper.load_model("medium", device="cuda:4")
+
+        self.model.encoder.to("cuda:4")
+        self.model.decoder.to("cuda:5")
+        self.model.decoder.register_forward_pre_hook(lambda _, inputs: tuple([inputs[0].to("cuda:5"), inputs[1].to("cuda:5")] + list(inputs[2:])))
+        self.model.decoder.register_forward_hook(lambda _, inputs, outputs: outputs.to("cuda:4"))
 
     def query_pdf(self, *, audio: str, add_tabs: bool) -> io.BytesIO:
         ...
@@ -14,5 +21,6 @@ class Backend:
         ...
 
     def query(self, *, audio: str) -> str:
+        text = self.model.transcribe(audio=audio)
         chords = autochord.recognize(audio)
-        return self.formatter.format(chords)
+        return self.formatter.format(chords, text)
